d1e761e01bc49e2f2dd7f04c78a886afa6365760
choijunghun <chjjh0@gmail.com>
2021-03-20 15:37:17 +0900

    react-motion-float-button 사용성테스트 준비

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Direction,
+  FloatMenuItemButton,
+  FloatingGroup,
+  Size,
+} from "react-button-from-junghun";
+import { Twitter } from "@emotion-icons/simple-icons";
+import { Facebook, Instagram, Share } from "@emotion-icons/remix-fill";
 
 function App() {
+  const handleButton = () => {
+    console.log("정훈");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="itemWrapper">
+        <FloatingGroup size={Size.REGULAR} direction={"column"}>
+          <FloatMenuItemButton
+            icon={<Twitter size="50%" />}
+            buttonColor="#00ACEE"
+          />
+          <FloatMenuItemButton
+            icon={<Instagram size="50%" />}
+            buttonColor="#4f5bd5"
+          />
+          <FloatMenuItemButton
+            icon={<Facebook size="50%" />}
+            buttonColor="#3B5998"
+          />
+          <FloatMenuItemButton
+            icon={<Share size="50%" />}
+            buttonColor="#16dbc2"
+          />
+        </FloatingGroup>
+      </div>
+    </>
   );
 }
 
